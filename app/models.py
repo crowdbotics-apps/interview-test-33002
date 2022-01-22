3dad75130cd7634a422afbb93b05cb3731f9e33a
Muhammad Hammad <hammadarshad834@gmail.com>
2022-01-23 02:46:03 +0500

    feat: create app model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Apps(models.Model):
+    WEB = 'web'
+    MOBILE = 'mobile'
+    DJANGO = 'django'
+    REACT_NATIVE = 'react native'
+
+    TYPE_CHOICES = (
+        (WEB, 'Web'),
+        (MOBILE, 'Mobile')
+    )
+
+    FRAMEWORK_CHOICES = (
+        (DJANGO, 'Django'),
+        (REACT_NATIVE, 'React Native')
+    )
+       
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(choices=TYPE_CHOICES, default=WEB, max_length=10)
+    framework = models.CharField(
+        choices=FRAMEWORK_CHOICES,
+        default=DJANGO,
+        max_length=20
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
